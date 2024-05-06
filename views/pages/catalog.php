704dbb9a103aeddafff2de8c26f0ed117f46058b
Nikita <nikiprostoff@gmail.com>
2024-05-06 19:04:11 +0500

    Каталог

--- views/pages/catalog.php
+++ views/pages/catalog.php
@@ -9,9 +9,27 @@
 
 <section class='catalog'>
 	<div class='wrapper'>
+		<h1 class="catalog__title">Каталог</h1>
+
 		<div class="catalog__inner">
-			<h1>Каталог</h1>
+			<div class="catalog__item">
+				<a href="./" class="catalog__hover">
+					<div class="catalog__price">
+						500000 РУБ
+					</div>
+					<div class="catalog__more">
+						<span>подробнее</span>
+						<img src="./assets/img/catalog/arrow.svg" alt="">
+					</div>
+					<img class="catalog__img" src="./assets/img/catalog/1.png" alt="">
+				</a>
+
+				<a href="./" class="catalog__button">
+					<span>зарядное устройство universal</span>
+				</a>
+			</div>
 		</div>
 	</div>
 </section>
+
 <?php $view->component('end') ?>
