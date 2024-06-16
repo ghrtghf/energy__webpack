d8a1b9a0b07d53007470ef78ce7df03605da42a8
Nikita <nikiprostoff@gmail.com>
2024-06-16 18:25:48 +0500

    last

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,7 +1,6 @@
 export default function animation() {
 	gsap.registerPlugin(ScrollTrigger);
 	const home = gsap.timeline();
-	const about = gsap.timeline();
 	const aboutTimeLine = gsap.timeline();
 
 	gsap.fromTo(
@@ -41,6 +40,32 @@ export default function animation() {
 			},
 		}
 	);
+	// let value = 1;
+	// const name = ['О нас', 'Каталог', 'Карта'];
+	// const navButton = document.querySelector('.navigation__active button p');
+	// const section = document.querySelectorAll('section');
+	// section.forEach(elem => {
+	// 	elem.className;
+	// });
+	// // const nameSection = section.className;
+	// let observer = new IntersectionObserver(
+	// 	(entries, observer) => {
+	// 		entries.forEach(entry => {
+	// 			if (entry.isIntersecting) {
+	// 				name.forEach((elem, index) => {
+	// 					navButton.innerHTML = `${elem}<span>${value++}<span>`;
+	// 				});
+	// 				// navButtonCount.innerHTML = value++;
+	// 				console.log(section);
+	// 				observer.unobserve(entry.target);
+	// 			}
+	// 		});
+	// 	}
+	// 	// { threshold: 1 }
+	// );
+	// document.querySelectorAll('section:nth-child(n+4)').forEach(sec => {
+	// 	observer.observe(sec);
+	// });
 
 	if (!sessionStorage.getItem('first')) {
 		gsap.fromTo('.home__title', { left: 500 }, { left: 0, duration: 2, delay: 3.4 });
@@ -60,7 +85,7 @@ export default function animation() {
 			.fromTo('.home__arrow', { scale: 0.6, opacity: 0, rotate: -50, autoAlpha: 0 }, { scale: 1, rotate: 0, opacity: 1, stagger: 0.2, duration: 1, autoAlpha: 1 }, '-=2.7')
 			.fromTo('.home__scroll', { opacity: 0, x: -50 }, { opacity: 1, x: 0, duration: 1 }, '-=2.6');
 	}
-	sessionStorage.setItem('first', 1);
+	// sessionStorage.setItem('first', 1);
 
 	aboutTimeLine.fromTo('.about__title', { opacity: 0, autoAlpha: 0, y: 400 }, { opacity: 1, autoAlpha: 1, y: 300 });
 
@@ -72,16 +97,38 @@ export default function animation() {
 			},
 			{
 				x: 0,
-			}
+			},
+			0.2
 		)
 		.fromTo(
-			'.text__span span',
+			'.text__left',
 			{
-				left: 80,
+				x: 20,
 			},
 			{
-				left: 0,
-			}
+				x: -10,
+			},
+			0.2
+		)
+		.fromTo(
+			'.text__right',
+			{
+				x: -15,
+			},
+			{
+				x: 0,
+			},
+			0.2
+		)
+		.fromTo(
+			'.text__right-large',
+			{
+				x: -45,
+			},
+			{
+				x: 0,
+			},
+			0.2
 		)
 		.fromTo(
 			'.about__svg',
@@ -104,7 +151,7 @@ export default function animation() {
 			0.8
 		)
 		.to(
-			'.about__title ',
+			'.about__title',
 			{
 				y: 50,
 			},
@@ -178,8 +225,30 @@ export default function animation() {
 
 	//////////////////////////////////////////////////////////////////
 	const catalog = gsap.timeline({ duration: 2 });
-
-	// const catalogTimeLine = gsap.timeline();
+	gsap.to('.catalog__img-left', {
+		opacity: 0,
+		autoAlpha: 0,
+		y: -500,
+		x: -100,
+		width: '400px',
+		height: '450px',
+	});
+	gsap.to('.catalog__img-right', {
+		opacity: 0,
+		autoAlpha: 0,
+		y: -500,
+		x: 800,
+		width: '400px',
+		height: '450px',
+	});
+	gsap.to('.catalog__img-center', {
+		opacity: 0,
+		autoAlpha: 0,
+		y: -100,
+		x: -360,
+		width: '500px',
+		height: '400px',
+	});
 	catalog
 		.fromTo(
 			'.catalog-home__title h2',
@@ -254,7 +323,7 @@ export default function animation() {
 				autoAlpha: 1,
 				scrollTrigger: {
 					trigger: '.catalog-home__wrapper',
-					start: '40% center',
+					start: '35% center',
 					end: '+=100',
 					scrub: 2,
 				},
@@ -264,7 +333,7 @@ export default function animation() {
 	ScrollTrigger.create({
 		animation: catalog,
 		trigger: '.catalog-home__wrapper',
-		start: 'center center',
+		start: '40% center',
 		end: '+=1000',
 		scrub: true,
 		anticipatePin: 1,
@@ -317,6 +386,42 @@ export default function animation() {
 				height: '400px',
 			},
 			1.4
+		)
+		.to(
+			'.catalog__img-left',
+			{
+				y: -500,
+				x: 15,
+				width: '400px',
+				height: '450px',
+				opacity: 1,
+				autoAlpha: 1,
+			},
+			1.6
+		)
+		.to(
+			'.catalog__img-right',
+			{
+				y: -500,
+				x: 580,
+				width: '400px',
+				height: '450px',
+				opacity: 1,
+				autoAlpha: 1,
+			},
+			1.6
+		)
+		.to(
+			'.catalog__img-center',
+			{
+				y: -320,
+				x: -360,
+				width: '500px',
+				height: '400px',
+				opacity: 1,
+				autoAlpha: 1,
+			},
+			1.6
 		);
 
 	new Swiper('.home__swiper', {
